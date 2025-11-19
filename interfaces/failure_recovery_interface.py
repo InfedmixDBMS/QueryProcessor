94f7b975e2724467a58f1fbbaef1ba3b684bb14c
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2025-11-19 11:47:19 +0700

    fix: function definitions in AbstractFailureRecoveryManager

--- interfaces/failure_recovery_interface.py
+++ interfaces/failure_recovery_interface.py
@@ -1,24 +1,18 @@
 from abc import ABC, abstractmethod
+from models.execution_result import ExecutionResult
+from models.recover_criteria import RecoverCriteria
 
 
 class AbstractFailureRecoveryManager(ABC):
     
     @abstractmethod
-    def log_query(self, transaction_id: str, query: str) -> None:
+    def write_log(info: ExecutionResult) -> None:
+        pass
+
+    @abstractmethod
+    def save_checkpoint() -> None:
         pass
     
     @abstractmethod
-    def log_transaction_start(self, transaction_id: str) -> None:
-        pass
-    
-    @abstractmethod
-    def log_transaction_commit(self, transaction_id: str) -> None:
-        pass
-    
-    @abstractmethod
-    def log_transaction_abort(self, transaction_id: str) -> None:
-        pass
-    
-    @abstractmethod
-    def recover(self) -> bool:
+    def recover(criteria: RecoverCriteria) -> None:
         pass
---
+++ models/recover_criteria.py
@@ -0,0 +1,4 @@
+from abc import ABC
+
+class RecoverCriteria(ABC):
+    pass
