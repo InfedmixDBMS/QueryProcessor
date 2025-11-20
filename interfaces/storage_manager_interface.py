4f814ce731217a8310af7867835c0dc2143a0656
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2025-11-20 20:51:38 +0700

    fix: storage_manager_interface functions (menyesuaikan dengan integration)

--- interfaces/storage_manager_interface.py
+++ interfaces/storage_manager_interface.py
@@ -5,33 +5,49 @@ from ..models import Rows
 
 class AbstractStorageManager(ABC):
     
+    # DataRetrieval: 
+    #   table -> str, 
+    #   column -> List[str], 
+    #   condition -> List[Condition]
+
+    # Condition: 
+    #   column -> str, 
+    #   operation -> enum('=', '<>', '>', '<', '>=', '<='), 
+    #   operand -> int | str | float
+
     @abstractmethod
-    def read_table(self, table_name: str, columns: Optional[List[str]] = None) -> Rows:
+    def read_block(self, data_retrieval: DataRetrieval) -> Rows:
         pass
     
+    # DataWrite: 
+    #   table -> str,
+    #   column -> List[str],
+    #   conditions -> List[Condition],
+    #   new_value -> List[T] | None
+
     @abstractmethod
-    def write_table(self, table_name: str, rows: Rows) -> int:
+    def write_block(self, data_write: DataWrite) -> int:
         pass
+
+    # DataDeletion: 
+    #   table -> str,
+    #   conditions -> List[Condition]
     
     @abstractmethod
-    def update_rows(
-        self, 
-        table_name: str, 
-        updates: Dict[str, Any], 
-        condition: Optional[Dict[str, Any]] = None
-    ) -> int:
+    def delete_block(self, data_deletion: DataDeletion) -> int:
         pass
+
+    # Schema:
+    #   columns:
+    #       "id" = IntType(),
+    #       "name" = VarCharType(50),
+    #       "ipk" = FloatType()
+    #   column_order: list(columns.keys())
+    #   column_index: {name: i for i, name in enumerate(self.column_order)}
+    #   size: int (auto-calculated, ga perlu dihitung manual)
     
     @abstractmethod
-    def delete_rows(self, table_name: str, condition: Optional[Dict[str, Any]] = None) -> int:
-        pass
-    
-    @abstractmethod
-    def insert_rows(self, table_name: str, rows: Rows) -> int:
-        pass
-    
-    @abstractmethod
-    def create_table(self, table_name: str, schema: Dict[str, str]) -> bool:
+    def create_table(self, table_name: str, schema: Schema) -> bool:
         pass
     
     @abstractmethod
